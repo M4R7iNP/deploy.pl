16a3a5df3ec54821e434ea541545265860026ff4
Martin Pedersen @ Mudkip <martin@m4r7.in>
2014-09-03 01:25:22 +0000

    Im too lazy to add cpan modules

--- deploy.pl
+++ deploy.pl
@@ -6,7 +6,6 @@ use Cwd;
 use File::chdir;
 use Git::Repository;
 use Data::Dumper;
-use List::MoreUtils 'any';
 
 my $repo;
 if($ARGV[0] && -d "/var/www/$ARGV[0]"){
@@ -24,7 +23,7 @@ my $git = Git::Repository->new();
 $git->run('pull');
 
 my @diff = $git->run('diff', '--name-only', 'HEAD^1', 'HEAD');
-if( any{ /^(js|css)\// } @diff ){
+if( /^(js|css)\// ~~ @diff ){
 	if(-f 'gulpfile.js'){
 		system('gulp prod');
 	}elsif(-f 'Gruntfile.js'){
