276f583c5fe9cbea3db7024156cebc5bb2dc3c42
Martin Pedersen @ Mudkip <martin@m4r7.in>
2014-11-16 00:28:19 +0000

    Pull submodules, npm install, chown css, dont override env

--- deploy.pl
+++ deploy.pl
@@ -20,15 +20,21 @@ $CWD = "/var/www/$repo";
 
 my $git = Git::Repository->new();
 
+$git->run('submodule', 'foreach', 'git', 'pull');
 $git->run('pull');
 
+#TODO: Diff between previous HEAD and HEAD, not only HEAD and prevous commit
 my @diff = $git->run('diff', '--name-only', 'HEAD^1', 'HEAD');
+if( /^package\.json$/ ~~ @diff ){
+	system('npm install');
+}
 if( /^(js|css)\// ~~ @diff ){
 	if(-f 'gulpfile.js'){
 		system('gulp prod');
 	}elsif(-f 'Gruntfile.js'){
 		system('grunt prod');
 	}
+	system('chown 1000:33 public/css -R');
 }
 
-system('naught deploy index.ipc');
+system('naught deploy --override-env false naught.ipc') if(-e 'naught.ipc');
