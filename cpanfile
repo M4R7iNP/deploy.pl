a96538956890f2648be5a887d5a9644500a99a55
Martin Pedersen <martin@m4r7.in>
2016-04-26 09:09:55 +0000

    commit many changes

---
+++ cpanfile
@@ -0,0 +1,5 @@
+requires 'Switch', '> 0';
+requires 'Cwd', '> 0';
+requires 'File::chdir', '> 0';
+requires 'Git::Repository', '> 0';
+requires 'Data::Dumper', '> 0';
--- deploy.pl
+++ deploy.pl
@@ -8,38 +8,64 @@ use Git::Repository;
 use Data::Dumper;
 
 my $repo;
-if($ARGV[0] && -d "/srv/www/$ARGV[0]"){
-	$repo = $ARGV[0];
-}elsif(getcwd() =~ /^\/srv\/www\/([\w\d\.]+)$/){
-	$repo = $1;
-}else{
-	$ARGV[0] = '' if not $ARGV[0];
-	die "Error: Repo '$ARGV[0]' not found!";
+my $force;
+
+while (my $arg = shift @ARGV) {
+    switch ($arg) {
+        case '--force' {
+            $force = shift @ARGV;
+        }
+        else {
+            if (!$repo && -d "/srv/www/$arg") {
+                $repo = $arg;
+            }
+        }
+    }
+}
+
+if (!$repo) {
+    if (getcwd() =~ /^\/srv\/www\/([\w\d\.]+)$/) {
+        $repo = $1;
+    } else {
+        die "Error: Repo not found!";
+    }
 }
 
 $CWD = "/srv/www/$repo";
 
 my $git = Git::Repository->new();
+my $preCommitId = $git->run('rev-parse', 'HEAD');
 
-$git->run('submodule', 'foreach', 'git', 'pull');
+#$git->run('submodule', 'update', '--init', '--recursive') unless scalar glob('./models/*'); #Doesnt work :c
+#$git->run('submodule', 'update', '--init', '--recursive'); #still doesnt work
+#$git->run('submodule', 'foreach', 'git', 'pull');
+$git->run('submodule', 'init');
+$git->run('submodule', 'update', '--init', '--recursive'); #still doesnt work
 $git->run('pull');
 
-#TODO: Diff between previous HEAD and HEAD, not only HEAD and prevous commit
-my @diff = $git->run('diff', '--name-only', 'HEAD^1', 'HEAD');
-if( /^package\.json$/ ~~ @diff ){
-	system('npm install');
-}
-if( /^(js|css)\// ~~ @diff ){
-	if(-f 'gulpfile.js'){
-		system('gulp prod');
-	}elsif(-f 'Gruntfile.js'){
-		system('grunt prod');
-	}
-	system('chown 1000:33 public/css -R');
+if($git->run('rev-parse', 'HEAD') eq $preCommitId) {
+    $preCommitId = 'HEAD~';
 }
 
-if(-e 'naught.ipc'){
-	system('naught deploy --override-env false naught.ipc');
+my @diff = $git->run('diff', '--name-only', $preCommitId, 'HEAD');
+if (grep(/^package\.json$/, @diff) || !-e 'node_modules' ) {
+    system('npm install --production') && die('npm install failed. Check output, resolve errors, run npm install again and then deploy again.');
+}
+if (grep(/^(js|css)\//, @diff) || !-e 'public/js' || !-e 'public/css' || ($force && $force eq 'gulp')) {
+    if (-f 'gulpfile.js') {
+        system('gulp prod');
+    }
+    elsif (-f 'Gruntfile.js') {
+        system('grunt prod');
+    }
+    system('chown 1000:33 public/css -R');
+    system('chown 1000:33 public/js -R');
+}
+
+if (-e 'naught.ipc') {
+    system('naught deploy --override-env false naught.ipc');
 } else {
-	system('NODE_ENV=production naught start index.js');
+    system('NODE_ENV=production naught start index.js');
+    #chown((getpwnam('martin'))[2], (getpwnam('www-data'))[2], 'naught.ipc');
+    #chmod 660, 'naught.ipc';
 }
